124f5ebebffceafa45bc3d8a7a078fb8f94dca67
Felix <thor4_000@10.238.33.207>
2014-10-23 09:52:39 -0400

    Dernier commit, version qui a fait le parcours

--- src/Armus.cpp
+++ src/Armus.cpp
@@ -12,8 +12,8 @@
 #include <stdint.h>
 
 const short PAUSE = 500;
-const short TIMEOUT = 50;//5;
-const short SUBDIVISIONS = 10;//100;
+const short TIMEOUT = 25;//50;
+const short SUBDIVISIONS = 20;//10;
 const short TICK_PER_ROTATION = 64;
 const short CM_PER_ROTATION = 24;
 const float PI = 3.1415926535897932384626;
@@ -24,18 +24,19 @@ void move_ticks(short, short);
 void turn_degrees(short, short);
 void turn_ticks(short, short);
 
+const short move_speed = 60;//70;
+const short turn_speed = 45;//50;
+
 int main()
 {
-	short move_speed = 60;
-	short turn_speed = 40;
-
 	THREAD_MSleep(PAUSE);
 
-	move_cm(220, move_speed);		THREAD_MSleep(PAUSE);
+	LCD_ClearAndPrint("");
+	move_cm(225, move_speed);		THREAD_MSleep(PAUSE);
 
 	turn_degrees(90, -turn_speed);	THREAD_MSleep(PAUSE);
 
-	move_cm(45/*50*/, move_speed);	THREAD_MSleep(PAUSE);
+	move_cm(50, move_speed);		THREAD_MSleep(PAUSE);
 
 	turn_degrees(90, turn_speed);	THREAD_MSleep(PAUSE);
 
@@ -43,7 +44,7 @@ int main()
 
 	turn_degrees(90, turn_speed);	THREAD_MSleep(PAUSE);
 
-	move_cm(45/*50*/, move_speed);	THREAD_MSleep(PAUSE);
+	move_cm(50, move_speed);		THREAD_MSleep(PAUSE);
 
 	turn_degrees(90, -turn_speed);	THREAD_MSleep(PAUSE);
 
@@ -61,9 +62,147 @@ int main()
 
 	move_cm(40, move_speed);		THREAD_MSleep(PAUSE);
 
-	turn_degrees(22, turn_speed);	THREAD_MSleep(PAUSE);
+	turn_ticks(4, turn_speed);		THREAD_MSleep(PAUSE);
+	//turn_degrees(22, turn_speed);	THREAD_MSleep(PAUSE);
 
 	move_cm(100, move_speed);
+
+	return 0;
+}
+
+void move_cm(short cm, short ticks_per_second)
+{
+	move_ticks((cm * TICK_PER_ROTATION) / CM_PER_ROTATION, ticks_per_second);
+}
+
+void move_ticks(short ticks, short ticks_per_second)
+{
+	short ticks_per_second_wanted = (ticks_per_second * TIMEOUT * SUBDIVISIONS) / 1000;
+
+	short ticks_left = 0;
+	short ticks_right = 0;
+
+	short speed_left = 50;//ticks_per_second;
+	short speed_right = 50;//ticks_per_second;
+
+	ENCODER_Read(ENCODER_LEFT);
+	ENCODER_Read(ENCODER_RIGHT);
+
+	while(1)
+	{
+		MOTOR_SetSpeed(MOTOR_LEFT,  speed_left);
+		MOTOR_SetSpeed(MOTOR_RIGHT, speed_right);
+
+		short progression_left = 0;
+		short progression_right = 0;
+
+		for(short i = 0; i < SUBDIVISIONS; i++)
+		{
+			THREAD_MSleep(TIMEOUT);
+
+			int temp_left  = ENCODER_Read(ENCODER_LEFT);
+			int temp_right = ENCODER_Read(ENCODER_RIGHT);
+
+			progression_left  += temp_left;
+			progression_right += temp_right;
+
+			ticks_left  += temp_left;
+			ticks_right += temp_right;
+
+			if(ticks_left >= ticks || ticks_right >= ticks)
+			{
+				MOTOR_SetSpeed(MOTOR_LEFT, 	0);
+				MOTOR_SetSpeed(MOTOR_RIGHT, 0);
+
+				//LCD_Printf("%d : %d-%d | ",ticks , ticks_left, ticks_right);
+
+				return;
+			}
+		}
+
+		speed_left  += (ticks_per_second_wanted - progression_left);
+		speed_right += (ticks_per_second_wanted - progression_right);
+
+		LCD_ClearAndPrint("%d_%d", speed_left, speed_right);
+	}
+}
+
+void turn_degrees(short degrees, short speed)
+{
+	turn_ticks((degrees * CM_BETWEEN * PI * TICK_PER_ROTATION) / (CM_PER_ROTATION * 360), speed);
+}
+
+void turn_ticks(short ticks, short speed)
+{
+	short ticks_left = 0;
+	short ticks_right = 0;
+
+	ENCODER_Read(ENCODER_LEFT);
+	ENCODER_Read(ENCODER_RIGHT);
+
+	MOTOR_SetSpeed(MOTOR_LEFT,  speed);
+	MOTOR_SetSpeed(MOTOR_RIGHT, -speed);
+
+	do
+	{
+		THREAD_MSleep(1);
+
+		ticks_left  += ENCODER_Read(ENCODER_LEFT);
+		ticks_right += ENCODER_Read(ENCODER_RIGHT);
+
+		if(ticks_left >= ticks) {
+			MOTOR_SetSpeed(MOTOR_LEFT, 0);
+		}
+
+		if(ticks_right >= ticks) {
+			MOTOR_SetSpeed(MOTOR_RIGHT, 0);
+		}
+
+	} while(ticks_left < ticks || ticks_right < ticks);
+
+	MOTOR_SetSpeed(MOTOR_LEFT, 	0);
+	MOTOR_SetSpeed(MOTOR_RIGHT, 0);
+
+	//LCD_Printf("%d : %d-%d\n",ticks , ticks_left, ticks_right);
+}
+
+/*int main()
+{
+	//THREAD_MSleep(PAUSE);
+
+	move_cm(213, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(90, MOTOR_RIGHT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(36, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(90, MOTOR_LEFT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(31, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(90, MOTOR_LEFT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(36, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(90, MOTOR_RIGHT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(21, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(45, MOTOR_LEFT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(37, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(90, MOTOR_RIGHT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(60, move_speed);		//THREAD_MSleep(PAUSE);//Moins loin!
+
+	turn_degrees(45, MOTOR_LEFT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(38, move_speed);		//THREAD_MSleep(PAUSE);
+
+	turn_degrees(22, MOTOR_LEFT);	//THREAD_MSleep(PAUSE);
+
+	move_cm(80, move_speed);
 
 	return 0;
 }
@@ -121,61 +260,26 @@ void move_ticks(short ticks, short ticks_per_second)
 	}
 }
 
-/*void move_ticks(short ticks, short ticks_per_second)
+void turn_degrees(short degrees, short motor)
 {
-	short ticks_per_second_wanted = (ticks_per_second * TIMEOUT) / 1000;
-
-	short ticks_left = 0;
-	short ticks_right = 0;
-
-	short speed_left = 50;//ticks_per_second;
-	short speed_right = 50;//ticks_per_second;
-
-	ENCODER_Read(ENCODER_LEFT);
-	ENCODER_Read(ENCODER_RIGHT);
-
-	do
-	{
-		MOTOR_SetSpeed(MOTOR_RIGHT, speed_right);
-
-		THREAD_MSleep(TIMEOUT);
-
-		ticks_left  += ENCODER_Read(ENCODER_LEFT);
-		ticks_right += ENCODER_Read(ENCODER_RIGHT);
-
-		speed_right += (ticks_left - ticks_right);
-
-	} while(ticks_left < ticks || ticks_right < ticks);
-
-	MOTOR_SetSpeed(MOTOR_LEFT, 	0);
-	MOTOR_SetSpeed(MOTOR_RIGHT, 0);
-}*/
-
-void turn_degrees(short degrees, short speed)
-{
-	turn_ticks((degrees * CM_BETWEEN * PI * TICK_PER_ROTATION) / (CM_PER_ROTATION * 360), speed);
+	turn_ticks((degrees * 2 * CM_BETWEEN * PI * TICK_PER_ROTATION) / (CM_PER_ROTATION * 360), motor);
 }
 
-void turn_ticks(short ticks, short speed)
+void turn_ticks(short ticks, short motor)
 {
-	short ticks_left = 0;
-	short ticks_right = 0;
+	short ticks_done = 0;
 
-	ENCODER_Read(ENCODER_LEFT);
-	ENCODER_Read(ENCODER_RIGHT);
+	ENCODER_Read(motor - 6);
 
-	MOTOR_SetSpeed(MOTOR_LEFT,  speed);
-	MOTOR_SetSpeed(MOTOR_RIGHT, -speed);
+	MOTOR_SetSpeed(motor,  turn_speed);
 
 	do
 	{
 		THREAD_MSleep(1);
 
-		ticks_left  += ENCODER_Read(ENCODER_LEFT);
-		ticks_right += ENCODER_Read(ENCODER_RIGHT);
+		ticks_done  += ENCODER_Read(motor - 6);
 
-	} while(ticks_left < ticks && ticks_right < ticks);
+	} while(ticks_done < ticks);
 
-	MOTOR_SetSpeed(MOTOR_LEFT, 	0);
-	MOTOR_SetSpeed(MOTOR_RIGHT, 0);
-}
+	MOTOR_SetSpeed(motor, 	0);
+}*/
